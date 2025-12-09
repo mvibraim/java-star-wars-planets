003765c69937e54c991539167db752951c6fb4ca
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2025-12-09 20:32:01 -0300

    Use DTO as record

--- src/main/java/com/example/starwarsplanets/dto/PlanetDTO.java
+++ src/main/java/com/example/starwarsplanets/dto/PlanetDTO.java
@@ -2,38 +2,7 @@ package com.example.starwarsplanets.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public class PlanetDTO {
-
-  @NotBlank(message = "Name cannot be empty")
-  private String name;
-
-  @NotBlank(message = "Terrain cannot be empty")
-  private String terrain;
-
-  @NotBlank(message = "Climate cannot be empty")
-  private String climate;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getTerrain() {
-    return terrain;
-  }
-
-  public void setTerrain(String terrain) {
-    this.terrain = terrain;
-  }
-
-  public String getClimate() {
-    return climate;
-  }
-
-  public void setClimate(String climate) {
-    this.climate = climate;
-  }
+public record PlanetDTO(@NotBlank(message = "Name cannot be empty") String name,
+    @NotBlank(message = "Terrain cannot be empty") String terrain,
+    @NotBlank(message = "Climate cannot be empty") String climate) {
 }
